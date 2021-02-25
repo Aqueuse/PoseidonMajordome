8df72b9ca3660393fe5880faf63aa39bfcc18aa4
Aqueuse <SR388-Aqueuse@users.noreply.github.com>
2021-02-25 16:58:07 +0100

    merge the app menu with the blockly and pane

--- static/index.js
+++ static/index.js
@@ -1,15 +1,26 @@
-var sideMove = "";
-var scriptBar = document.getElementById("blocklyContainerID");
-var draghandleRight = document.getElementById("draghandleRight");
-var sidebarRight = document.getElementById("sidebarRight");
-var diptychWidth = window.innerWidth / 2;
+////////////////// pan management //////////////////////////////
+function onresize(e) {
+  var blocklyDiv = document.getElementById('blocklyDiv');
+
+  // Position blocklyDiv over blocklyContainer
+  blocklyDiv.style.left = '0px';
+  blocklyDiv.style.top = '0px';
+  blocklyDiv.style.width = "100%";
+  blocklyDiv.style.height = "100%";
+  Blockly.svgResize(workspace);
+};
 
 function barsInitialPlacement() {
-  scriptBar.style.left = "0px";
-  scriptBar.style.width = diptychWidth + "px";
-  draghandleRight.style.left = diptychWidth + "px";
-  sidebarRight.style.left = diptychWidth + "px";
-  sidebarRight.style.width = diptychWidth + "px";
+  var scriptPan = document.getElementById("editorView");
+  var draghandle = document.getElementById("draghandle");
+  var plotPan = document.getElementById("plotView");
+  var diptychWidth = window.innerWidth / 2;
+
+  scriptPan.style.left = "0px";
+  scriptPan.style.width = diptychWidth + "px";
+  draghandle.style.left = diptychWidth + "px";
+  plotPan.style.left = diptychWidth + "px";
+  plotPan.style.width = diptychWidth + "px";
   onresize();
 }
 
@@ -21,13 +32,15 @@ function initResize(e) {
 function Resize(e) {
   onresize();
 
-  sidebarRight.style.left = e.clientX + "px";
-  sidebarRight.style.width = window.innerWidth - e.clientX + "px";
-  draghandleRight.style.left = e.clientX + "px";
+  var scriptPan = document.getElementById("editorView");
+  var plotPan = document.getElementById("plotView");
+  plotPan.style.left = e.clientX + "px";
+  plotPan.style.width = window.innerWidth - e.clientX + "px";
+  draghandle.style.left = e.clientX + "px";
 
-  scriptBar.style.width = parseInt(window.innerWidth, 10)
+  scriptPan.style.width = parseInt(window.innerWidth, 10)
     - (
-      parseInt(sidebarRight.style.width, 10)
+      parseInt(plotPan.style.width, 10)
       ) + "px";
   }
 
@@ -36,3 +49,27 @@ function stopResize(e) {
   window.removeEventListener("mousemove", Resize, false);
   window.removeEventListener("mouseup", stopResize, false);
 }
+
+
+////////////// tab management /////////////////
+
+function openTab(tabID, element, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  document.getElementById(tabID).style.display = "block";
+  element.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
